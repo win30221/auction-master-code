4eff859c76a079e486c4919a8f3682ccf1452d5b
win30221 <win30221@yahoo.com.tw>
2024-04-27 13:03:10 +0800

    feature: 刪除無用欄位

--- const/wallets/wallets.go
+++ const/wallets/wallets.go
@@ -3,10 +3,8 @@ package wallets
 import "time"
 
 type Wallet struct {
-	ID          *uint64    `json:"id"`
 	ConsignorID *uint64    `form:"consignorID" json:"consignorID"`
 	Balance     *float64   `form:"balance" json:"balance"`
-	Status      *uint8     `form:"status" json:"status"`
 	CreatedAt   *time.Time `form:"createdAt" json:"createdAt"`
 	UpdatedAt   *time.Time `form:"updatedAt" json:"updatedAt"`
 }
