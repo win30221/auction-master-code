c44de2e3c0e40eb25b66699135ac948a0b6d3007
win30221 <win30221@yahoo.com.tw>
2024-04-30 21:48:53 +0800

    fix: 修正欄位

--- const/items/items.go
+++ const/items/items.go
@@ -26,7 +26,7 @@ type Item struct {
 	MinEstimatedPrice *int       `form:"minEstimatedPrice" json:"minEstimatedPrice"`
 	MaxEstimatedPrice *int       `form:"maxEstimatedPrice" json:"maxEstimatedPrice"`
 	ReservePrice      *int       `form:"reservePrice" json:"reservePrice"`
-	ExpireAt          *time.Time `form:"createdAt" json:"expireAt"`
+	ExpireAt          *time.Time `form:"expireAt" json:"expireAt"`
 	Status            *uint8     `form:"status" json:"status"`
 	CreatedAt         *time.Time `form:"createdAt" json:"createdAt"`
 	UpdatedAt         *time.Time `form:"updatedAt" json:"updatedAt"`
