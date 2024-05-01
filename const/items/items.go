af415406a5e7f8f88cb9696b2443893279a1c3ba
win30221 <win30221@yahoo.com.tw>
2024-05-01 10:22:24 +0800

    feature: 增加 item 結構

--- const/items/items.go
+++ const/items/items.go
@@ -16,6 +16,23 @@ var (
 
 )
 
+type ItemDetails struct {
+	ID                *uint64    `json:"id"`
+	ConsignorID       *uint64    `json:"consignorID"`
+	Type              *uint8     `json:"type"`
+	Name              *string    `json:"name"`
+	Description       *string    `json:"description"`
+	Photo             *string    `json:"photo"`
+	Space             *uint8     `json:"space"`
+	MinEstimatedPrice *int       `json:"minEstimatedPrice"`
+	MaxEstimatedPrice *int       `json:"maxEstimatedPrice"`
+	ReservePrice      *int       `json:"reservePrice"`
+	ExpireAt          *time.Time `json:"expireAt"`
+	Status            *uint8     `json:"status"`
+	CreatedAt         *time.Time `json:"createdAt"`
+	UpdatedAt         *time.Time `json:"updatedAt"`
+}
+
 type Item struct {
 	ID                *uint64    `form:"id" json:"id"`
 	ConsignorID       *uint64    `form:"consignorID" json:"consignorID"`
@@ -30,4 +47,12 @@ type Item struct {
 	Status            *uint8     `form:"status" json:"status"`
 	CreatedAt         *time.Time `form:"createdAt" json:"createdAt"`
 	UpdatedAt         *time.Time `form:"updatedAt" json:"updatedAt"`
+}
+
+type ItemPhoto struct {
+	ItemID    *uint64    `form:"itemID" json:"itemID"`
+	Index     *uint8     `form:"index" json:"index"`
+	URL       *string    `form:"url" json:"url"`
+	CreatedAt *time.Time `form:"createdAt" json:"createdAt"`
+	UpdatedAt *time.Time `form:"updatedAt" json:"updatedAt"`
 }
