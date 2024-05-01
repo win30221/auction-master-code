ba5e337e3393ee3e1e48babc8c2c4f15a39711aa
win30221 <win30221@yahoo.com.tw>
2024-05-01 21:08:00 +0800

    Update items.go

--- const/items/items.go
+++ const/items/items.go
@@ -19,20 +19,20 @@ var (
 )
 
 type ItemDetails struct {
-	ID                *uint64    `json:"id"`
-	ConsignorID       *uint64    `json:"consignorID"`
-	Type              *uint8     `json:"type"`
-	Name              *string    `json:"name"`
-	Description       *string    `json:"description"`
-	Photo             []string   `json:"photo"`
-	Space             *uint8     `json:"space"`
-	MinEstimatedPrice *int       `json:"minEstimatedPrice"`
-	MaxEstimatedPrice *int       `json:"maxEstimatedPrice"`
-	ReservePrice      *int       `json:"reservePrice"`
-	ExpireAt          *time.Time `json:"expireAt"`
-	Status            *uint8     `json:"status"`
-	CreatedAt         *time.Time `json:"createdAt"`
-	UpdatedAt         *time.Time `json:"updatedAt"`
+	ID                *uint64     `json:"id"`
+	ConsignorID       *uint64     `json:"consignorID"`
+	Type              *uint8      `json:"type"`
+	Name              *string     `json:"name"`
+	Description       *string     `json:"description"`
+	Photos            []ItemPhoto `json:"photos"`
+	Space             *uint8      `json:"space"`
+	MinEstimatedPrice *int        `json:"minEstimatedPrice"`
+	MaxEstimatedPrice *int        `json:"maxEstimatedPrice"`
+	ReservePrice      *int        `json:"reservePrice"`
+	ExpireAt          *time.Time  `json:"expireAt"`
+	Status            *uint8      `json:"status"`
+	CreatedAt         *time.Time  `json:"createdAt"`
+	UpdatedAt         *time.Time  `json:"updatedAt"`
 }
 
 type Item struct {
@@ -52,11 +52,11 @@ type Item struct {
 }
 
 type ItemPhoto struct {
-	ItemID    *uint64    `form:"itemID" json:"itemID"`
+	ItemID    *uint64    `form:"itemID" json:"itemID,omitempty"`
 	Index     *uint8     `form:"index" json:"index"`
 	URL       *string    `form:"url" json:"url"`
-	CreatedAt *time.Time `form:"createdAt" json:"createdAt"`
-	UpdatedAt *time.Time `form:"updatedAt" json:"updatedAt"`
+	CreatedAt *time.Time `form:"createdAt" json:"createdAt,omitempty"`
+	UpdatedAt *time.Time `form:"updatedAt" json:"updatedAt,omitempty"`
 }
 
 type GetItemDetailsRes struct {
