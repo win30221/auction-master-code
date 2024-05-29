1fdc8c4ce0338e2903861b88345ae25d498ace30
win30221 <win30221@yahoo.com.tw>
2024-05-29 11:36:55 +0800

    Update opcode.go

--- const/opcode/opcode.go
+++ const/opcode/opcode.go
@@ -1,11 +1,14 @@
 package opcode
 
 const (
-	Deal = "Deal-Item:%d-AuctionItem:%s"
+	Deal   = "Deal-Item:%d-AuctionItem:%s"
+	PayFee = "PayFee-Item:%d-AuctionItem:%s"
 )
 
 const (
 	WalletSoldType = "1000"
+
+	WalletPayFeeType = "2000"
 )
 
 const (
