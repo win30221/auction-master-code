2364d3fc2b4aac59de1ef36e680d726f4223acc7
win30221 <win30221@yahoo.com.tw>
2024-09-01 08:06:42 +0800

    feat: 增加欄位

--- const/reports/reports.go
+++ const/reports/reports.go
@@ -47,34 +47,42 @@ type Report struct {
 }
 
 type Record struct {
-	ID                   *primitive.ObjectID `json:"id" bson:"_id"`
-	Type                 *int                `form:"type" json:"type" bson:"type" sendForm:"type"`
-	ConsignorID          *uint64             `form:"consignorID" json:"consignorID" bson:"consignorID" sendForm:"consignorID"`
-	ConsignorNickname    *string             `form:"consignorNickname" json:"consignorNickname" bson:"consignorNickname" sendForm:"consignorNickname"`
-	OpCode               *string             `form:"opCode" json:"opCode" bson:"opCode" sendForm:"opCode"`
-	ItemID               *uint64             `form:"itemID" json:"itemID" bson:"itemID" sendForm:"itemID"`
-	AuctionItemID        *uint64             `form:"auctionItemID" json:"auctionItemID,omitempty" bson:"auctionItemID,omitempty" sendForm:"auctionItemID"`
-	Currency             *string             `form:"currency" json:"currency,omitempty" bson:"currency,omitempty" sendForm:"currency"`
-	ExchangeRate         *float64            `form:"exchangeRate" json:"exchangeRate,omitempty" bson:"exchangeRate,omitempty" sendForm:"exchangeRate"`
-	JpyWithdrawal        *int                `form:"jpyWithdrawal" json:"jpyWithdrawal,omitempty" bson:"jpyWithdrawal,omitempty" sendForm:"jpyWithdrawal"`
-	Withdrawal           *int                `form:"withdrawal" json:"withdrawal,omitempty" bson:"withdrawal,omitempty" sendForm:"withdrawal"`
-	ClosedPrice          *int                `form:"closedPrice" json:"closedPrice,omitempty" bson:"closedPrice,omitempty" sendForm:"closedPrice"`
-	Price                *int                `form:"price" json:"price,omitempty" bson:"price,omitempty" sendForm:"price"`
-	DirectPurchasePrice  *int                `form:"directPurchasePrice" json:"directPurchasePrice,omitempty" bson:"directPurchasePrice,omitempty" sendForm:"directPurchasePrice"`
-	PurchasedPrice       *int                `form:"purchasedPrice" json:"purchasedPrice,omitempty" bson:"purchasedPrice,omitempty" sendForm:"purchasedPrice"`
-	YahooAuctionFeeRate  *float64            `form:"yahooAuctionFeeRate" json:"yahooAuctionFeeRate,omitempty" bson:"yahooAuctionFeeRate,omitempty" sendForm:"yahooAuctionFeeRate"`
-	YahooAuctionFee      *int                `form:"yahooAuctionFee" json:"yahooAuctionFee,omitempty" bson:"yahooAuctionFee,omitempty" sendForm:"yahooAuctionFee"`
-	CommissionRate       *float64            `form:"commissionRate" json:"commissionRate,omitempty" bson:"commissionRate,omitempty" sendForm:"commissionRate"`
-	Commission           *int                `form:"commission" json:"commission,omitempty" bson:"commission,omitempty" sendForm:"commission"`
-	BonusRate            *float64            `form:"bonusRate" json:"bonusRate,omitempty" bson:"bonusRate,omitempty" sendForm:"bonusRate"`
-	Bonus                *int                `form:"bonus" json:"bonus,omitempty" bson:"bonus,omitempty" sendForm:"bonus"`
-	Profit               *int                `form:"profit" json:"profit,omitempty" bson:"profit,omitempty" sendForm:"profit"`
-	YahooCancellationFee *int                `form:"yahooCancellationFee" json:"yahooCancellationFee,omitempty" bson:"yahooCancellationFee,omitempty" sendForm:"yahooCancellationFee"`
-	SpaceFee             *int                `form:"spaceFee" json:"spaceFee,omitempty" bson:"spaceFee,omitempty" sendForm:"spaceFee"`
-	ShippingCost         *int                `form:"shippingCost" json:"shippingCost,omitempty" bson:"shippingCost,omitempty" sendForm:"shippingCost"`
-	Status               *uint8              `form:"status" json:"status" bson:"status" sendForm:"status"`
-	CreatedAt            *time.Time          `json:"createdAt" bson:"createdAt"`
-	UpdatedAt            *time.Time          `json:"updatedAt" bson:"updatedAt"`
+	ID                *primitive.ObjectID `json:"id" bson:"_id"`
+	Type              *int                `form:"type" json:"type" bson:"type" sendForm:"type"`
+	ConsignorID       *uint64             `form:"consignorID" json:"consignorID" bson:"consignorID" sendForm:"consignorID"`
+	ConsignorNickname *string             `form:"consignorNickname" json:"consignorNickname" bson:"consignorNickname" sendForm:"consignorNickname"`
+	OpCode            *string             `form:"opCode" json:"opCode" bson:"opCode" sendForm:"opCode"`
+	ItemID            *uint64             `form:"itemID" json:"itemID" bson:"itemID,omitempty" sendForm:"itemID"`
+	AuctionItemID     *uint64             `form:"auctionItemID" json:"auctionItemID,omitempty" bson:"auctionItemID,omitempty" sendForm:"auctionItemID"`
+	Currency          *string             `form:"currency" json:"currency,omitempty" bson:"currency,omitempty" sendForm:"currency"`
+	ExchangeRate      *float64            `form:"exchangeRate" json:"exchangeRate,omitempty" bson:"exchangeRate,omitempty" sendForm:"exchangeRate"`
+
+	// ===提款相關===
+	JpyWithdrawal *int    `form:"jpyWithdrawal" json:"jpyWithdrawal,omitempty" bson:"jpyWithdrawal,omitempty" sendForm:"jpyWithdrawal"`
+	Withdrawal    *int    `form:"withdrawal" json:"withdrawal,omitempty" bson:"withdrawal,omitempty" sendForm:"withdrawal"`
+	BankCode      *string `form:"bankCode" json:"bankCode" bson:"bankCode,omitempty" sendForm:"bankCode"`
+	BankAccount   *string `form:"bankAccount" json:"bankAccount" bson:"bankAccount,omitempty" sendForm:"bankAccount"`
+
+	// ===結標相關===
+	ClosedPrice         *int     `form:"closedPrice" json:"closedPrice,omitempty" bson:"closedPrice,omitempty" sendForm:"closedPrice"`
+	Price               *int     `form:"price" json:"price,omitempty" bson:"price,omitempty" sendForm:"price"`
+	DirectPurchasePrice *int     `form:"directPurchasePrice" json:"directPurchasePrice,omitempty" bson:"directPurchasePrice,omitempty" sendForm:"directPurchasePrice"`
+	PurchasedPrice      *int     `form:"purchasedPrice" json:"purchasedPrice,omitempty" bson:"purchasedPrice,omitempty" sendForm:"purchasedPrice"`
+	YahooAuctionFeeRate *float64 `form:"yahooAuctionFeeRate" json:"yahooAuctionFeeRate,omitempty" bson:"yahooAuctionFeeRate,omitempty" sendForm:"yahooAuctionFeeRate"`
+	YahooAuctionFee     *int     `form:"yahooAuctionFee" json:"yahooAuctionFee,omitempty" bson:"yahooAuctionFee,omitempty" sendForm:"yahooAuctionFee"`
+	CommissionRate      *float64 `form:"commissionRate" json:"commissionRate,omitempty" bson:"commissionRate,omitempty" sendForm:"commissionRate"`
+	Commission          *int     `form:"commission" json:"commission,omitempty" bson:"commission,omitempty" sendForm:"commission"`
+	BonusRate           *float64 `form:"bonusRate" json:"bonusRate,omitempty" bson:"bonusRate,omitempty" sendForm:"bonusRate"`
+	Bonus               *int     `form:"bonus" json:"bonus,omitempty" bson:"bonus,omitempty" sendForm:"bonus"`
+	Profit              *int     `form:"profit" json:"profit,omitempty" bson:"profit,omitempty" sendForm:"profit"`
+
+	// ===未結標相關===
+	YahooCancellationFee *int       `form:"yahooCancellationFee" json:"yahooCancellationFee,omitempty" bson:"yahooCancellationFee,omitempty" sendForm:"yahooCancellationFee"`
+	SpaceFee             *int       `form:"spaceFee" json:"spaceFee,omitempty" bson:"spaceFee,omitempty" sendForm:"spaceFee"`
+	ShippingCost         *int       `form:"shippingCost" json:"shippingCost,omitempty" bson:"shippingCost,omitempty" sendForm:"shippingCost"`
+	Status               *uint8     `form:"status" json:"status" bson:"status" sendForm:"status"`
+	CreatedAt            *time.Time `json:"createdAt" bson:"createdAt"`
+	UpdatedAt            *time.Time `json:"updatedAt" bson:"updatedAt"`
 }
 
 type CreateReportReq struct {
