e76ad62bff4db1bc05fdd0a5a812eb361c45e3e5
win30221 <win30221@yahoo.com.tw>
2024-09-24 11:22:26 +0800

    Update shippings.go

--- const/shippings/shippings.go
+++ const/shippings/shippings.go
@@ -27,21 +27,21 @@ var (
 )
 
 type Shipping struct {
-	ID                       *primitive.ObjectID `json:"id" bson:"_id"`
-	ActionType               *uint8              `form:"actionType" json:"actionType" bson:"actionType"`
-	ShipmentType             *uint8              `form:"shipmentType" json:"shipmentType" bson:"shipmentType"`
-	ItemIDs                  *[]uint64           `form:"itemIDs" json:"itemIDs" bson:"itemIDs"`
-	AuctionItemIDs           *[]uint64           `form:"auctionItemIDs" json:"auctionItemIDs,omitempty" bson:"auctionItemIDs,omitempty"`
-	Address                  *string             `form:"address" json:"address,omitempty" bson:"address,omitempty"`
-	StoreNumber              *string             `form:"storeNumber" json:"storeNumber,omitempty" bson:"storeNumber,omitempty"`
-	StoreName                *string             `form:"storeName" json:"storeName,omitempty" bson:"storeName,omitempty"`
-	RecipientName            *string             `form:"recipientName" json:"recipientName" bson:"recipientName"`
-	Phone                    *string             `form:"phone" json:"phone" bson:"phone"`
-	ShipmentTrackingNumber   *string             `form:"shipmentTrackingNumber" json:"shipmentTrackingNumber" bson:"shipmentTrackingNumber"`
-	ShippingCostsWithinJapan *int                `form:"shippingCostsWithinJapan" json:"shippingCostsWithinJapan,omitempty" bson:"shippingCostsWithinJapan,omitempty"`
-	Status                   *uint8              `form:"status" json:"status" bson:"status"`
-	CreatedAt                *time.Time          `json:"createdAt" bson:"createdAt"`
-	UpdatedAt                *time.Time          `json:"updatedAt" bson:"updatedAt"`
+	ID                         *primitive.ObjectID `json:"id" bson:"_id"`
+	ActionType                 *uint8              `form:"actionType" json:"actionType" bson:"actionType"`
+	ShipmentType               *uint8              `form:"shipmentType" json:"shipmentType" bson:"shipmentType"`
+	ItemIDs                    *[]uint64           `form:"itemIDs" json:"itemIDs" bson:"itemIDs"`
+	AuctionItemIDs             *[]uint64           `form:"auctionItemIDs" json:"auctionItemIDs,omitempty" bson:"auctionItemIDs,omitempty"`
+	Address                    *string             `form:"address" json:"address,omitempty" bson:"address,omitempty"`
+	StoreNumber                *string             `form:"storeNumber" json:"storeNumber,omitempty" bson:"storeNumber,omitempty"`
+	StoreName                  *string             `form:"storeName" json:"storeName,omitempty" bson:"storeName,omitempty"`
+	RecipientName              *string             `form:"recipientName" json:"recipientName" bson:"recipientName"`
+	Phone                      *string             `form:"phone" json:"phone" bson:"phone"`
+	ShipmentTrackingNumber     *string             `form:"shipmentTrackingNumber" json:"shipmentTrackingNumber" bson:"shipmentTrackingNumber,omitempty"`
+	InternationalShippingCosts *int                `form:"internationalShippingCosts" json:"internationalShippingCosts,omitempty" bson:"internationalShippingCosts,omitempty"`
+	Status                     *uint8              `form:"status" json:"status" bson:"status"`
+	CreatedAt                  *time.Time          `json:"createdAt" bson:"createdAt"`
+	UpdatedAt                  *time.Time          `json:"updatedAt" bson:"updatedAt"`
 }
 
 type GetShippingsReq struct {
